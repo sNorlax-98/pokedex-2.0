2dc4ba1b8f0747164be98d4ef5cdab7b4f3047f9
snorlax-98m <Anupamdas19980@gmail.com>
2023-04-17 03:40:23 +0530

    fixed fav page for each pokemon details

--- pokedex/src/pages/Favourite.jsx
+++ pokedex/src/pages/Favourite.jsx
@@ -1,76 +1,65 @@
-import React from "react";
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import Header from "../components/Header";
 import searchTermContext from "../context/searchTermContext";
 import favimg from "../assets/favimg.png";
 import Stats from "./Stats";
 import Moves from "./Moves";
 import Forms from "./Forms";
+
 const Favourite = () => {
-  const [showdDetails, setShowDetails] = useState(false);
-  const {
-    favPokemon,
-    setfavPokemon,
-    show,
-    removeFavoritePokemon,
-    setShow,
-    selectedPokemon,
-  } = useContext(searchTermContext);
+  const { favPokemon, removeFavoritePokemon, setShow } =
+    useContext(searchTermContext);
+  const [showDetails, setShowDetails] = useState({});
+
+  const toggleDetails = (id) => {
+    setShowDetails((prevState) => ({ ...prevState, [id]: !prevState[id] }));
+  };
+
   return (
     <>
       <div className="fav-tab">
         <Header />
-        {show
-          ? favPokemon.map((pokemon) => {
-              return (
-                <div className="card-group">
-                  <div
-                    className={
-                      showdDetails ? "favcard-expand" : "favcard-retend"
-                    }
-                  >
-                    <h1>{pokemon.name}</h1>
+        {favPokemon.length > 0 &&
+          favPokemon.map((pokemon) => {
+            const { id, name, sprites } = pokemon;
+            const isShowDetails = showDetails[id];
+            return (
+              <div className="card-group" key={id}>
+                <div
+                  className={
+                    isShowDetails ? "favcard-expand" : "favcard-retend"
+                  }
+                >
+                  <h1>{name}</h1>
+                  <img
+                    className="poke-img"
+                    src={sprites.other.home.front_default}
+                    alt="poke-img"
+                  />
+                  <button onClick={() => removeFavoritePokemon(pokemon, id)}>
                     <img
-                      className="poke-img"
-                      src={pokemon.sprites.other.home.front_default}
-                      alt="poke-img"
+                      className="toogle-img"
+                      src={favimg}
+                      alt="remove-favorite"
                     />
-                    <button
-                      onClick={() => {
-                        setShow(false);
-                        removeFavoritePokemon(pokemon, pokemon.id);
-                      }}
-                    >
-                      <img className="toogle-img" src={favimg} />
-                    </button>
-                    {showdDetails ? (
-                      <button
-                        onClick={() => setShowDetails(!showdDetails)}
-                        className="btn"
-                      >
-                        click to hide details
-                      </button>
-                    ) : (
-                      <button
-                        onClick={() => setShowDetails(!showdDetails)}
-                        className="btn"
-                      >
-                        click for details
-                      </button>
-                    )}
-
-                    {showdDetails ? (
-                      <>
-                        <Stats />
-                        <Moves />
-                        <Forms />
-                      </>
-                    ) : null}
-                  </div>
+                  </button>
+                  <button onClick={() => toggleDetails(id)} className="btn">
+                    {isShowDetails
+                      ? "click to hide details"
+                      : "click for details"}
+                  </button>
+                  {isShowDetails && (
+                    <>
+                      <Stats />
+                      <Moves />
+                      <Forms />
+                    </>
+                  )}
                 </div>
-              );
-            })
-          : null}
+              </div>
+            );
+          })}
+        {favPokemon.length === 0 && <p>No favorite Pokemon found!</p>}
       </div>
     </>
   );
